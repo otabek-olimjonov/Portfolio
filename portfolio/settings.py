f529cd2617bb2682265909d82383b28293979e9b
Otabek Olimjonov <56047755+Otabek0626@users.noreply.github.com>
2021-05-25 19:35:47 -0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -36,8 +36,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'testwebbackend@gmail.com'
-EMAIL_HOST_PASSWORD = 'test@web101'
+EMAIL_HOST_USER = '*******'
+EMAIL_HOST_PASSWORD = '******'
 
 
 # Application definition
